c9e171bf6b8885b062b8257cb774143d793b68a0
Richard Dzenis <dzenis@richard.lv>
2022-09-14 13:12:26 +0300

    [AST] Add msvc-specific C++11 attribute 'msvc::noop_dtor'
    This attribute first appeared in MSVC toolchain 14.28.29333
    
    Relates to #57696

--- clang/test/AST/msvc-attrs.cpp
+++ clang/test/AST/msvc-attrs.cpp
@@ -1,5 +1,5 @@
 // RUN: %clang_cc1 -fms-extensions -DMSX -ast-dump %s | FileCheck %s
-// RUN: not %clang_cc1 -Werror=ignored-attributes -ast-dump %s 2>&1 | grep "4 errors generated"
+// RUN: not %clang_cc1 -Werror=ignored-attributes -ast-dump %s 2>&1 | grep "5 errors generated"
 
 // CHECK: VarDecl 0x{{[0-9a-f]+}} <{{.*}}:[[@LINE+3]]:24, col:47> col:47 no_tls_guard_var
 // CHECK-NEXT: MSNoTlsGuardAttr 0x{{[0-9a-f]+}} <col:3, col:9>
@@ -14,3 +14,10 @@ struct [[msvc::known_semantics]] Struct {};
 // CHECK-NEXT: BuiltinType 0x{{[0-9a-f]+}} 'int'
 // CHECK-NEXT: MSKnownSemanticsAttr 0x{{[0-9a-f]+}} <col:13, col:19>
 using Int [[msvc::known_semantics]] = int;
+
+struct Dtor {
+  // CHECK: CXXDestructorDecl 0x{{[0-9a-f]+}} <line:[[@LINE+3]]:23, col:32> col:23 ~Dtor 'void () noexcept'
+  // CHECK-NEXT: CompoundStmt 0x{{[0-9a-f]+}} <col:31, col:32>
+  // CHECK-NEXT: MSNoopDtorAttr 0x{{[0-9a-f]+}} <col:5, col:11>
+  [[msvc::noop_dtor]] ~Dtor() {}
+};
